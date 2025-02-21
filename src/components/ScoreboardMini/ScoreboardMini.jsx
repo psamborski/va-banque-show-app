0ebc229e280024ff374baa3cc21cef2a26f2b70a
psamborski <psambek@gmail.com>
2025-02-22 00:37:42 +0100

    Added mini scoreboard for convenient balance change.

---
+++ src/components/ScoreboardMini/ScoreboardMini.jsx
@@ -0,0 +1,64 @@
+import React, {useState} from "react"
+
+import {usePlayers} from "../../context/PlayersContext"
+
+import "./ScoreboardMini.css"
+
+const ScoreboardMini = () => {
+    const [moneyChange, setMoneyChange] = useState('')
+    const [selectedPlayerId, setSelectedPlayerId] = useState('')
+
+    const {players, updateBalance} = usePlayers()
+
+    const handleBalanceChange = (type = 'add') => {
+        const parsedBalance = parseInt(moneyChange, 10)
+        if (!isNaN(parsedBalance)) {
+            if (type === 'subtract') {
+                updateBalance(selectedPlayerId, players.find(p => p.id === selectedPlayerId).balance - parsedBalance)
+            } else {
+                updateBalance(selectedPlayerId, players.find(p => p.id === selectedPlayerId).balance + parsedBalance)
+            }
+        }
+
+        setMoneyChange('')
+    }
+
+    return <div className="scoreboard-mini-container">
+        {
+            players.map((player) => (
+                    <div
+                        className="scoreboard-mini-player"
+                        key={`scb-mini-${player.id}`}
+                    >
+                        <span className={'scoreboard-mini-name'}>{player.name}</span>
+
+                        <div className="scoreboard-mini-balance-container">
+                            <input
+                                type="number"
+                                value={selectedPlayerId === player.id ? moneyChange : ''}
+                                onFocus={() => setSelectedPlayerId(player.id)}
+                                onChange={(e) => setMoneyChange(e.target.value)}
+                                onKeyDown={(e) => e.key === "Enter" && handleBalanceChange()}
+                                autoFocus
+                            />
+                            <button
+                                className={'scoreboard-mini-btn add-money-btn'}
+                                onClick={() => handleBalanceChange()}
+                            >
+                                +
+                            </button>
+                            <button
+                                className={'scoreboard-mini-btn subtract-money-btn'}
+                                onClick={() => handleBalanceChange('subtract')}
+                            >
+                                -
+                            </button>
+                        </div>
+                    </div>
+                )
+            )
+        }
+    </div>
+}
+
+export default ScoreboardMini
