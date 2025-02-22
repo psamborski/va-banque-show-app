de15be5c6ed21a8e6e0c27127f977d9474d5aa26
psamborski <psambek@gmail.com>
2025-02-22 11:50:37 +0100

    Players data in localStorage.

--- src/components/TimesUpSoundPlayer/TimesUpSoundPlayer.jsx
+++ src/components/TimesUpSoundPlayer/TimesUpSoundPlayer.jsx
@@ -1,12 +1,11 @@
 import React, {useEffect, useState} from "react"
-import PropTypes from 'prop-types'
 import ReactPlayer from "react-player"
 
 import timesUpSound from '../../assets/audio/times-up.mp3'
 
 const TimesUpSoundPlayer = () => {
     const [playing, setPlaying] = useState(false)
-    
+
     useEffect(() => {
         const handleKeyPress = (event) => {
             if (event.key.toLowerCase() === "t") {
